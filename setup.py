239b5554757dd9a2152930c15bf4c010217f2bd7
Fabian Neumann <fn@sturzbach.de>
2010-04-07 19:09:01 +0200

    more comprehensive setup.py; added changelog and prepare for 0.1 release

--- setup.py
+++ setup.py
@@ -1,10 +1,36 @@
-from setuptools import setup, find_packages
+import os, sys
+from setuptools import setup
 
-setup(name='bitlyapi',
-        version='0.1dev',
-        description='A thin wrapper for the bit.ly REST api',
-        author='Lars Kellogg-Stedman',
-        author_email='lars@oddbit.com',
-        packages=['bitlyapi'],
-        scripts=['scripts/bitly',]
-        )
+version = '0.1'
+
+install_requires = ['setuptools']
+if sys.version_info < (2, 6):
+    install_requires.append('simplejson')
+
+setup(
+    name='bitlyapi',
+    version=version,
+    description='A thin wrapper for the bit.ly REST API',
+    long_description='\n\n'.join([
+        open("README.rst").read(),
+        open("CHANGES.rst").read(),
+    ]),
+    url = 'http://github.com/hellp/bitlyapi',
+    license = 'BSD',
+    author='Fabian Neumann',
+    author_email='fn@sturzbach.de',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=['bitlyapi'],
+    scripts=['scripts/bitly',],
+    install_requires=install_requires,
+)
