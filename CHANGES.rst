3e7988569a491b240c9bfcecdabbacb6fb66facb
Fabian Neumann <fn@sturzbach.de>
2010-04-07 21:04:32 +0200

    release 0.1.1: fixed packaging

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@
 Changelog
 =========
 
+0.1.1 (2010-04-07)
+==================
+
+- fixed path to README and CHANGES in setup.py and add MANIFEST.in
+
+
 0.1 (2010-04-07)
 ================
 
--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 import os, sys
 from setuptools import setup
 
-version = '0.1'
+version = '0.1.1'
 
 install_requires = ['setuptools']
 if sys.version_info < (2, 6):
     install_requires.append('simplejson')
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name='bitlyapi',
     version=version,
     description='A thin wrapper for the bit.ly REST API',
-    long_description='\n\n'.join([
-        open("README.rst").read(),
-        open("CHANGES.rst").read(),
-    ]),
+    long_description='\n\n'.join([read("README.rst"), read("CHANGES.rst")]),
     url = 'http://github.com/hellp/bitlyapi',
     license = 'BSD',
     author='Fabian Neumann',
